70ee44e7a7295d53478175145651497d91be1547
Jean Pougetoux <Jean.Pougetoux@fmc-ag.com>
2018-11-21 16:09:59 +0100

    update datas (adding more randomness)

--- MachineLearningTests/Program.cs
+++ MachineLearningTests/Program.cs
@@ -14,18 +14,23 @@ namespace MachineLearningTests
             var tree = classifier.BuildTree(Datas.trainingDatas);
             classifier.PrintTree(tree);
 
-            foreach(var datas in Datas.testingDatas)
+            int numbError = 0;
+
+
+            foreach (var datas in Datas.testingDatas)
             {
-                int numbError = 0;
                 Console.WriteLine("excepted : " + datas[Datas.labelIndex]);
                 string name = classifier.Classify(datas, tree);
                 Console.WriteLine(name);
 
                 if (name.Split(' ')[0].ToString() != datas[Datas.labelIndex].ToString())
                 {
+                    Console.WriteLine("" + datas[0] + " " + datas[1] + " " + datas[2] + " " + datas[3] + " " + datas[4] + " ");
                     numbError++;
                 }
             }
+
+            Console.WriteLine("" + numbError + " errors.");
 
             Console.ReadLine();
         }
@@ -36,7 +41,7 @@ namespace MachineLearningTests
 
             List<object[]> objects = new List<object[]>();
 
-            for (int i = 0; i < 75; i++)
+            for (int i = 0; i < 70; i++)
             {
                 var splittedLine = lines[i].Split(',');
                 objects.Add(new object[] { double.Parse(splittedLine[0].Replace('.', ',')), double.Parse(splittedLine[1].Replace('.', ',')), double.Parse(splittedLine[2].Replace('.', ',')), double.Parse(splittedLine[3].Replace('.', ',')), splittedLine[4] });
@@ -45,7 +50,7 @@ namespace MachineLearningTests
             Datas.trainingDatas = objects.ToArray();
             objects = new List<object[]>();
 
-            for (int i = 75; i < 150; i++)
+            for (int i = 70; i < 150; i++)
             {
                 var splittedLine = lines[i].Split(',');
                 objects.Add(new object[] { double.Parse(splittedLine[0].Replace('.', ',')), double.Parse(splittedLine[1].Replace('.', ',')), double.Parse(splittedLine[2].Replace('.', ',')), double.Parse(splittedLine[3].Replace('.', ',')), splittedLine[4] });
